09cfeade63a5e9fd8f1ecd688b2516e93c49a963
litun098 <nayaklitun9@gmail.com>
2024-04-30 21:56:50 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+
+import { app } from "./app.js";
+
 import connectDB from "./db/index.js";
 
-import dotenv from 'dotenv'
 
 dotenv.config({
-    path:"./env"
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running at port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB Connection failed!!!", err);
+  });
