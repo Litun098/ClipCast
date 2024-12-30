880d68541dfaa142eb2fae99054d1a07c7db4e4b
litun098 <nayaklitun9@gmail.com>
2024-12-30 21:00:06 +0530

    fixed a bug

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,8 +9,23 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
 
+  const alreadySubscribed = await Subscription.findOne({
+    subscriber: req.user._id,
+    channel: channelId,
+  });
+
+  if (alreadySubscribed) {
+    await Subscription.findOneAndDelete({ subscriber: req.user._id });
+
+    return res.status(200).json(new ApiResponse(200, "Unsubscribed,"));
+  }
+
   // Query using _id (if that's the correct field for the channel)
-  const channel = await User.findOne({ _id: channelId });
+  const channel = await User.findOne({ _id: channelId }).select(
+    "username avatar _id"
+  );
+
+  console.log(channel);
 
   if (!channel) {
     throw new ApiError(404, "Channel not found");
@@ -18,7 +33,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
   const subscribe = await Subscription.create({
     subscriber: req.user._id,
-    channel: channel._Id,
+    channel: channel._id,
   });
 
   return res
@@ -27,12 +42,12 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 });
 
 // controller to return subscriber list of a channel
-const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
 });
 
 // controller to return channel list to which user has subscribed
-const getSubscribedChannels = asyncHandler(async (req, res) => {
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
 });
 
